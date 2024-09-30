862ef298b82ff6b8150fb6621d59e4aadd03badb
Daniel <danielalamba15@gmail.com>
2024-09-30 18:30:51 +0100

    mobile test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment={
-    apiUrl: 'http://127.0.0.1:8000/api'
+    // apiUrl: 'http://127.0.0.1:8000/api'
+   apiUrl: 'https://aandbcleaner.org/phoenix/Phoenix-api/public/api'
     // 
 }
