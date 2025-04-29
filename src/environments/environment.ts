81f21fe5c182e0b5a5201cba34947e85c8da0fe3
Daniel Alamba <danielalamba15@gmail.com>
2025-04-29 16:19:57 +0100

    updated endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment={
     // apiUrl: 'http://127.0.0.1:8000/api'
-   apiUrl: 'https://aandbcleaner.org/phoenix/Phoenix-api/public/api'
+   apiUrl: 'https://coestudycenter.com.ng/phoenix/Phoenix-api/public/api'
     // 
 }
