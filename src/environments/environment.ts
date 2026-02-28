66929d08d63f567a1c0860bbf702ebc026d39492
Alamba <danielalamba15@gmail.com>
2026-02-28 05:54:52 +0100

    updated environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8080/api',
-    // apiUrl: 'https://coestudycenter.com.ng/phoenix/Phoenix-api/public/api',  
-    // apiUrl: 'https://aandbcleaner.org/phoenix/Phoenix-api/public/api',
-    pusher: {
-        key: '45cde359e2dec89841a7',
-        cluster: 'mt1'
-    }
-}   
+  production: false,
+  apiUrl: "http://localhost:8000/api",
+  // apiUrl: 'https://coestudycenter.com.ng/phoenix/Phoenix-api/public/api',
+  // apiUrl: 'https://aandbcleaner.org/phoenix/Phoenix-api/public/api',
+  pusher: {
+    key: "45cde359e2dec89841a7",
+    cluster: "mt1",
+  },
+};
